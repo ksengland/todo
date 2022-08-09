e37d9e34721af6838f281804bcf10deb7b9744ba
Karleen England <karleenengland@runbox.com>
2022-08-09 10:27:21 -0500

    refactored as React app, with testing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Todo from "./components/todo";
+import TodoForm from "./components/form";
 
 function App() {
+  const [todos, setTodos] = React.useState([
+    {
+      text: "Wash dishes",
+      isCompleted: false,
+    },
+    {
+      text: "Fold laundry",
+      isCompleted: false,
+    },
+    {
+      text: "Take out trash",
+      isCompleted: false,
+    },
+  ]);
+
+  const addTodo = (text) => {
+    const newTodos = [...todos, { text: text, isCompleted: false }];
+    setTodos(newTodos);
+  };
+  const removeTodo = (index) => {
+    let temp = [...todos];
+    temp.splice(index, 1);
+    setTodos(temp);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="todo-list">
+        <h1>Things to Do</h1>
+        {todos.map((todo, i) => (
+          <Todo index={i} key={i} todo={todo} remove={removeTodo} />
+        ))}
+        <br></br>
+        <TodoForm addTodo={addTodo}></TodoForm>
+      </div>
     </div>
   );
 }
---
+++ src/components/todo.jsx
@@ -0,0 +1,16 @@
+function Todo({ todo, index, remove }) {
+    function handle() {
+      console.log("Ping:", index);
+      remove(index);
+    }
+    return (
+      <div className="todo" onClick={handle}>
+        <label>
+          <input type="checkbox" />
+          {todo.text}
+        </label>
+      </div>
+    );
+  }
+
+  export default Todo;
